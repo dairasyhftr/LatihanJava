8d678831bdc3a670c18e624b9afdf09747e9537f
dairasyhftr <dairasyahfitri14.gmail.com>
2024-04-07 11:18:39 +0700

    Pertemuan 2

---
+++ SepedaGunungBeraksi.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.latihan.java;
+
+/**
+ *
+ * @author DAIRA SYAHFITRI
+ */
+public class SepedaGunungBeraksi {
+    public static void main(String[] args) {
+// Mmbuat Object
+SepedaGunung sepedaku = new SepedaGunung();
+// Memanggil method di object
+    sepedaku.tambahKecepatan(10);
+    sepedaku.ubahGir(2);
+    sepedaku.setSadel(20);
+}
+}
