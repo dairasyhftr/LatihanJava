e6d3bdc6995111ca047f640ffe88de722bf7bd5e
dairasyhftr <dairasyahfitri14.gmail.com>
2024-04-07 11:56:19 +0700

    Pertemuan 1

---
+++ LatihanVar.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.latihan.java;
+
+/**
+ *
+ * @author DAIRA SYAHFITRI
+ */
+public class LatihanVar {
+    
+    public static void main(String[] args) {
+        //TODO code application logic here
+        
+        int JariJari;
+        double keliling;
+        final double PI = 3.14;
+        
+        JariJari = 30;
+        
+        keliling = 2*PI*JariJari;
+        System.out.println("Keliling Lingkaran = "+ keliling);
+        System.out.println("======================");
+    }
+}
