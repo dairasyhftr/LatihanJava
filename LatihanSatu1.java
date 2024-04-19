903238c10fbf6f43d8d26316d9c00f26c5416226
dairasyhftr <dairasyahfitri14.gmail.com>
2024-04-19 12:26:10 +0700

    Pertemuan 6

---
+++ LatihanSatu1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author DAIRA SYAHFITRI
+ */
+ class LatihanSatu1 {
+     
+     public static void main(String[]args) {
+         javax.swing.JFrame myWindow;
+         myWindow = new javax.swing.Jframe();
+         myWindow.setSize(300,200);
+         myWindow.setTitle("My First Java Program");
+         myWindow.setVisible(true);
+     }
+}
