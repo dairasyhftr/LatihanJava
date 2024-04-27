608462189a2842321ea53d87e0cf8748fa8cb1d6
dairasyhftr <dairasyahfitri14.gmail.com>
2024-04-27 19:13:52 +0700

    Pertemuan 2

---
+++ Pertemuan2/Belajar.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.latihan.java;
+
+/**
+ *
+ * @author DAIRA SYAHFITRI
+ */
+class Belajar 
+{
+public String X = "Pintar";
+public String Y = "Java";
+}
